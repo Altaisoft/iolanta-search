3b650138b5040929137756e3a91697b6b3b8d5e6
Anatoly Scherbakov <anatoly@criterion.net>
2019-05-31 00:43:57 +0700

    Category menu is a component

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 import { Iolanta } from "./iolanta";
 
+let TOKEN =
+    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OnlldGktcmVhZG9' +
+    'ubHkiLCJpc3MiOiJhZ2VudDp5ZXRpLXJlYWRvbmx5Ojo1Y2Q4YmE5Zi01ZGQ2LTQ4MWU' +
+    'tOWUxOC04NTIwYjIxN2I0Y2QiLCJpYXQiOjE1NTYyMDcwMjUsInJvbGUiOlsidXNlcl9' +
+    'hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZSw' +
+    'ic2FtbCI6e319.-7tuD6sdMhjEADykZ0QoMWAUfoXcHXlkpS3KRmLtN465A6wuK2fVtu' +
+    '4y3OO2kqRcPZRkUMUGzAEi_LmCy5uQQQ';
+
+
 Vue.component('datadotworld-table', {
     props: ['query_id'],
     template: `
@@ -37,25 +46,12 @@ Vue.component('datadotworld-table', {
     methods: {
         update: function() {
             // noinspection SpellCheckingInspection
-            let app = this,
-                token =
-                    'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50OnlldGktcmVhZG9' +
-                    'ubHkiLCJpc3MiOiJhZ2VudDp5ZXRpLXJlYWRvbmx5Ojo1Y2Q4YmE5Zi01ZGQ2LTQ4MWU' +
-                    'tOWUxOC04NTIwYjIxN2I0Y2QiLCJpYXQiOjE1NTYyMDcwMjUsInJvbGUiOlsidXNlcl9' +
-                    'hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZSw' +
-                    'ic2FtbCI6e319.-7tuD6sdMhjEADykZ0QoMWAUfoXcHXlkpS3KRmLtN465A6wuK2fVtu' +
-                    '4y3OO2kqRcPZRkUMUGzAEi_LmCy5uQQQ';
+            let app = this;
 
-            new Iolanta(token).execute_stored_query(
+            new Iolanta(TOKEN).execute_stored_query(
                 app.query_id
             ).then(function (data) {
                 app.dataset = data;
-            });
-
-            new Iolanta(token).execute_stored_query(
-                'eafde27a-f163-41bb-a9cf-7a8a3f46a01c'
-            ).then(function (data) {
-                app.category_list = data.map(function(datum) { return datum.category });
             });
 
             /*
@@ -74,6 +70,34 @@ Vue.component('datadotworld-table', {
         this.update();
     }
 });
+
+
+Vue.component('category-menu', {
+    props: ['query_id'],
+    data: function() {
+        return {
+            dataset: null
+        }
+    },
+    template: `
+        <div>{{ dataset }}</div>
+    `,
+    methods: {
+        update: function() {
+            let app = this;
+            new Iolanta(TOKEN).execute_stored_query(
+                this.query_id
+            ).then(function (data) {
+                app.dataset = data.map(function(datum) { return datum.category });
+                console.log(app.dataset);
+            });
+        },
+    },
+    created: function () {
+        this.update();
+    }
+});
+
 
 let app = new Vue({
     el: '#app',
